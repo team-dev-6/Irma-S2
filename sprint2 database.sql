ba532d20f1ba7925f99e2d0721a147e5774f3413
irmagabriels <60925653+irmagabriels@users.noreply.github.com>
2020-02-19 12:04:15 +0200

    Add files via upload

---
+++ sprint2 database.sql
@@ -0,0 +1,22 @@
+Catergories	Brands	Cost Price	Prices	Stock	Brands Sold for Month	Profit
+Chips	Simba 125g	8	16	50	45	384
+Chips	Lays 125g	9	18	50	50	450
+Chips	Doritos 125g	8	16	50	48	384
+Cooldrink	Coke 2lt	10	20	100	100	1000
+Cooldrink	Fanta 2lt	9	18	100	90	810
+Cooldrink	Sprite 2lt	9	18	100	98	882
+Chocolates	Cadbury 50g	5	10	50	48	192
+Chocolates	Tex 50g	5	10	50	50	250
+Chocolates	PS 50g	5	10	50	38	190
+Pies	Pepper Steak	10	20	40	39	390
+Pies	Chicken	8	16	40	30	240
+Pies	Steak & Kidney	10	20	40	40	400
+Fruit	Pear	6	12	50	45	270
+Fruit	Apple	6	12	50	40	240
+Fruit	Orange	5	10	50	39	195
+Cupcakes	Chocolate (6)	9	18	100	95	1710
+Cupcakes	Vanilla (6)	7	14	100	80	560
+Cupcakes	Blueberry (6)	10	20	100	100	1000
+Veg	Spinach	8	16	30	25	200
+Veg	Cabbage	8	16	30	30	240
+Veg	Tomato	5	10	30	30	150
